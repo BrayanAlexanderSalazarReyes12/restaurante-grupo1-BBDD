83cd11bb7316ac4f95d8b74a5b7ff4ea3dd1bac4
BrayanAlexanderSalazarReyes12 <brayanreyes2008@hotmail.com>
2021-11-25 12:46:57 -0500

    aperitivos

--- restaurante-grupo1-BBDD/Controllers/AperitivosController.cs
+++ restaurante-grupo1-BBDD/Controllers/AperitivosController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using MySql.Data.MySqlClient;
@@ -10,8 +11,8 @@ using System.Text.RegularExpressions;
 
 namespace restaurante_grupo1_BBDD.Controllers
 {
+    [Route("api/[controller]")]
     [ApiController]
-    [Route("[controller]")]
     public class AperitivosController : ControllerBase
     {
         private readonly IConfiguration _configuration;
